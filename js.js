7f8d9498e6abd1af585b717fe08100b7a4bb9e2d
giovanna1012 <giovanna.joaquim12@gmail.com>
2020-09-24 18:48:09 -0300

    Missao estelar js realizada com sucesso

---
+++ js.js
@@ -0,0 +1,9 @@
+var missao= "Missao estelar JS em execusão";
+
+function eventoOnLoad(){
+    console.log("eventoOnload disparado");
+}
+
+function eventoOnclick(){
+    console.log("Missão Estelar JS realizada com sucesso!!!");
+}
